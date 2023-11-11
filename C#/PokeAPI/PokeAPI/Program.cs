8a195fd21180071573499557bfa07d8a6218b5f8
Vitor Mattos <vitorfmattos03@gmail.com>
2023-11-11 17:14:48 -0300

    Capturando response em JSON por requisição get

---
+++ C#/PokeAPI/PokeAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using RestSharp;
+
+namespace PokeAPI
+{
+    internal class Program
+    {
+
+
+        static async Task Main(string[] args)
+        {
+
+            var options = new RestClientOptions("https://pokeapi.co")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/v2/pokemon/", Method.Get);
+            RestResponse response = await client.ExecuteAsync(request);
+            await Console.Out.WriteLineAsync(response.Content);
+
+        }
+
+    }
+}
