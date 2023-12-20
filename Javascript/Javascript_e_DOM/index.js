3883cfb5fa102e83b25671531555ae9109d514d6
Vitor Mattos <vitorfmattos03@gmail.com>
2023-12-20 00:27:14 -0300

    Aplicando validações e transition no header

--- Javascript/Javascript_e_DOM/index.js
+++ Javascript/Javascript_e_DOM/index.js
@@ -60,13 +60,51 @@ function editarLinha(dados) {
     var colunas = linha.getElementsByTagName("td");
 
 
-    var nomeNovo = prompt("Digite o novo nome");
-    var dataNovo = prompt("Digite a nova data");
+    var nomeNovo = obterNomeValido();
+    var dataNovo = obterDataValida();
 
     colunas[0].innerHTML = nomeNovo;
     colunas[1].innerHTML = dataNovo;
 
     saveData();
+}
+
+function obterNomeValido(){
+    var nome;
+    do{
+        nome = prompt("Digite o nome (Apenas letras):");
+    }while(!validarNome(nome));
+
+    return nome;
+}
+
+function obterDataValida(){
+    var data;
+    do{
+        data = prompt("Digite a data (dd/mm/aaaa):");
+    }while(!validarData(data));
+
+    return data;
+}
+
+function validarNome(nome){
+    var regex = /^[a-zA-Z\s]+$/;
+
+    if(!regex.test(nome)){
+        alert("Digite apenas letras!");
+    }
+
+    return regex.test(nome);
+}
+
+function validarData(data){
+    var regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+
+    if(!regex.test(data)){
+        alert("Formato de data inválida!");
+    }
+
+    return regex.test(data);
 }
 
 function deletarLinha(dados){
@@ -76,3 +114,14 @@ function deletarLinha(dados){
 
     saveData();
 }
+
+var header = document.getElementsByClassName("cabecalho")[0];
+window.onscroll = function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        if (window.innerWidth > 820) {
+            header.classList.add('scrolled');
+        }
+    } else {
+        header.classList.remove('scrolled');
+    }
+};
